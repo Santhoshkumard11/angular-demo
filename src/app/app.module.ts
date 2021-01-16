8fed73e749779de9dbce0f7a7a493fcab0974a8c
santhsohkumard11 <santhoshkdhana@gmail.com>
2021-01-16 22:24:26 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningComponent } from "./warning/warning.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
